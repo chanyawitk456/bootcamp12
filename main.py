9fa587a62d4f000da967edd7d911c00c4f34df14
chanyawitk456 <chanyawitk@gmail.com>
2026-02-27 03:18:51 +0000

    add add_two function

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #print hello word
 print("hello word")
 
+#print good bye
+print("good bye")
+
+#print new line
+print("hi")
+
+
+# new function
+def add_two(num):
+    return num + 2
+print(add_two(2))
